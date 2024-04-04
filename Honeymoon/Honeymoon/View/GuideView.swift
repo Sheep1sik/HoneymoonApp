88ace5874eb6e9834ad5dd174a9a208ed432ceb2
Sheep1sik <bohg6799@naver.com>
2024-04-04 16:42:46 +0900

    Feat: GuideView 구현

---
+++ Honeymoon/Honeymoon/View/GuideView.swift
@@ -0,0 +1,80 @@
+//
+//  GuideView.swift
+//  Honeymoon
+//
+//  Created by 양원식 on 4/4/24.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    // MARK: - PROPERTY
+    
+    // MARK: - FUNCTION
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false, content: {
+            VStack(alignment: .center, spacing: 20, content: {
+                HeaderComponent()
+                
+                Spacer(minLength: 10)
+                
+                Text("Get Started!")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.pink)
+                
+                Text("Discover and pick the perfect destination for your romantic Honeymoon!")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.center)
+                
+                Spacer(minLength: 10)
+                
+                VStack(alignment: .leading, spacing: 25, content: {
+                    GuideComponent(
+                        title: "Like",
+                        subtitle: "Swipe right",
+                        description: "Do you like this destination? Touch the screen and swipe right. It will be saved to the favourites.",
+                        icon: "heart.circle")
+                    
+                    GuideComponent(
+                        title: "Dismiss",
+                        subtitle: "Swipe left",
+                        description: "Would you rather skip this place? Touch the screen and swipe left. You will no longer see it.",
+                        icon: "xmark.circle")
+                    
+                    GuideComponent(
+                        title: "Book",
+                        subtitle: "Tap the button",
+                        description: "Our selection of honeymoon resorts is perfect setting for you to embark your new life together.",
+                        icon: "checkmark.square")
+                })
+                
+                Spacer(minLength: 10)
+                
+                Button(action: {
+                    print("A button was tapped.")
+                }, label: {
+                    Text("Continue".uppercased())
+                        .font(.headline)
+                        .padding()
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(Capsule().fill(Color.pink))
+                        .foregroundColor(Color.white)
+                })
+            })
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.top, 15)
+            .padding(.bottom, 25)
+            .padding(.horizontal, 25)
+        })
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    GuideView()
+}
