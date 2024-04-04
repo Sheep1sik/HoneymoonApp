fc9d351335b9f69c5e8ae780d66af25b4b85ea01
Sheep1sik <bohg6799@naver.com>
2024-04-04 15:34:27 +0900

    Feat: FooterView 구현

---
+++ Honeymoon/Honeymoon/View/FooterView.swift
@@ -0,0 +1,43 @@
+//
+//  FooterView.swift
+//  Honeymoon
+//
+//  Created by 양원식 on 4/4/24.
+//
+
+import SwiftUI
+
+struct FooterView: View {
+    var body: some View {
+        HStack(content: {
+            Image(systemName: "xmark.circle")
+                .font(.system(size: 42, weight: .light))
+            
+            Spacer()
+            
+            Button(action: {
+                print("Success!")
+            }, label: {
+                Text("Book Destination".uppercased())
+                    .font(.system(.subheadline, design: .rounded))
+                    .fontWeight(.heavy)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 12)
+                    .accentColor(Color.pink)
+                    .background(
+                        Capsule().stroke(Color.pink, lineWidth: 2)
+                    )
+            })
+            
+            Spacer()
+            
+            Image(systemName: "heart.circle")
+                .font(.system(size: 42, weight: .light))
+        })
+        .padding()
+    }
+}
+
+#Preview {
+    FooterView()
+}
