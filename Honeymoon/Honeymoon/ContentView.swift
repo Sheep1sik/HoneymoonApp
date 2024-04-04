b550aec9207f470a14baf5105112bc5b80d8aec7
Sheep1sik <bohg6799@naver.com>
2024-04-04 13:58:24 +0900

    Feat: Setup

---
+++ Honeymoon/Honeymoon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by 양원식 on 4/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by 양원식 on 4/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
