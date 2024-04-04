4cba482d3560a08565039f1214c23b08077ee06a
Sheep1sik <bohg6799@naver.com>
2024-04-04 15:10:29 +0900

    Feat: 추가 - CardView 구현 - HoneymoonModel 생성 - HoneymoomData 생성 - LaunchScreen 구현

---
+++ Honeymoon/Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by 양원식 on 4/4/24.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
